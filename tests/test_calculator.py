c4074f8f8a83f682384c764e69f3d47cd1a9c919
ainhoajimenez <167288809+ainhoajimenez@users.noreply.github.com>
2026-05-13 15:34:45 +0200

    Add modulo test cases to test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,14 @@
-from calculator import add, sub
+from calculator import add, sub, modulo
+
+
 def test_add():
-assert add(2, 3) == 5
+    assert add(2, 3) == 5
+
+
 def test_sub():
-assert sub(10, 4) == 6
+    assert sub(10, 4) == 6
+
+
+def test_modulo():
+    assert modulo(10, 3) == 1
+    assert modulo(8, 4) == 0
