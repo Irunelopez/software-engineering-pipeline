82dda8d6deb490b9583fe47a8a4a58366b7b01ed
Yihan2255 <167854313+Yihan2255@users.noreply.github.com>
2026-05-13 18:44:08 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,22 +2,30 @@
 def add(a, b):
     return a + b
 
+
 def sub(a, b):
   return a - b
 
+
 def squared(a):
     return a*a
+
+
 def truncate(value):
     return int(value)
     
+    
 def multiply(a, b):
     return a * b
+    
 
 def divide(a, b):
     return a / b
-  
+
+
 def modulo(a,b):
     return a % b
+
 
 def power(a,b):
     return a ** b
