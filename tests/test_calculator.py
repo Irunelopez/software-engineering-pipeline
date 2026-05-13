9c441ec7246e59f51f51f8be7abdb2a087fd53f4
Irunelopez <167971388+Irunelopez@users.noreply.github.com>
2026-05-13 16:01:48 +0200

    Add divide function test

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,4 @@
-from calculator import add, sub
+from calculator import add, sub, divide
 
 
 def test_add():
@@ -9,3 +9,6 @@ def test_sub():
     assert sub(10, 4) == 6
 
 
+def test_divide():
+    assert divide(6, 2) == 3
+    assert divide(8, 4) == 2
